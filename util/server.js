53511f35b09b41f9f188b2f8327e4f973d950760
Andrew Lunde <andrew.lunde@sap.com>
2019-07-31 15:54:56 -0400

    remove util auth req

--- util/server.js
+++ util/server.js
@@ -11,85 +11,15 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-function mtMiddleware(req, res, next) {
-	
-	// Assumes the HDI container was tagged with a tag of the form subdomain:<subdomain> and is bound to the MTAppBackend
-	var tagStr = "subdomain:" + req.authInfo.subdomain;
-	
-	// reqStr += "\nSearching for a bound hana container with tag: " + tagStr + "\n";
-	
-	var services = xsenv.getServices({
-		hana: { tag: tagStr }
-	});
-	
-	// If a container service binding was found
-	req.tenantContainer = services.hana;
-	// Else throw an error?  Not handled yet
-	//.catch(function (error) {
-	//	res.status(500).send(error.message);
-	//})
-	
-	next();
-}
-
-
-passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({uaa: {tag: "xsuaa"}}).uaa));
-app.use(passport.initialize());
-app.use(passport.authenticate("JWT", {session: false}));
-
 app.use(bodyParser.json());
-
-//app.use(mtMiddleware);
-
-// app functionality
-app.get("/", function (req, res) {
-	//var reqStr = stringifyObj(req, {
-	var reqStr = stringifyObj(req.authInfo.userInfo, {
-    indent: "   ",
-    singleQuotes: false
-	});
-
-	reqStr += "\n\n";
-
-	reqStr += stringifyObj(req.authInfo.scopes, {
-    indent: "   ",
-    singleQuotes: false
-	});
-
-	var responseStr = "";
-	responseStr += "<!DOCTYPE HTML><html><head><title>ConcileTime</title></head><body><h1>ConcileTime</h1><h2>Welcome " + req.authInfo.userInfo.givenName + " " + req.authInfo.userInfo.familyName + "!</h2><p><b>Subdomain:</b> " + req.authInfo.subdomain + "</p><br />";
-	responseStr += "<a href=\"/get_xxx\">/get_xxx</a><br />";
-	var isAuthorized = req.authInfo.checkScope(req.authInfo.xsappname + '.create');
-	if (isAuthorized) {
-		responseStr += "<a href=\"/add_xxx\">/add_legal_entity</a><br />";
-	}
-	responseStr += "<p><b>Identity Zone:</b> " + req.authInfo.identityZone + "</p><p><b>Origin:</b> " + req.authInfo.origin + "</p>" + "<br /><br /><pre>" + reqStr + "</pre>" + "</body></html>";
-	res.status(200).send(responseStr);
-});
 
 //subscribe/onboard a subscriber tenant
 app.get("/callback/v1.0/tenants/*", function (req, res) {
-	//var reqStr = stringifyObj(req, {
-	var reqStr = stringifyObj(req.authInfo.userInfo, {
-    indent: "   ",
-    singleQuotes: false
-});
-
-	reqStr += "\n\n";
-
-	reqStr += stringifyObj(req.authInfo.scopes, {
-    indent: "   ",
-    singleQuotes: false
-});
 
 	var responseStr = "";
-	responseStr += "<!DOCTYPE HTML><html><head><title>ConcileTime</title></head><body><h1>ConcileTime</h1><h2>Welcome " + req.authInfo.userInfo.givenName + " " + req.authInfo.userInfo.familyName + "!</h2><p><b>Subdomain:</b> " + req.authInfo.subdomain + "</p><br />";
-	responseStr += "Tenant callback endpoint only allows PUT and DELETE methods.<br />";
-	var isAuthorized = req.authInfo.checkScope(req.authInfo.xsappname + '.create');
-	if (isAuthorized) {
-		responseStr += "<a href=\"/add_legal_entity\">/add_legal_entity</a><br />";
-	}
-	responseStr += "<p><b>Identity Zone:</b> " + req.authInfo.identityZone + "</p><p><b>Origin:</b> " + req.authInfo.origin + "</p>" + "<br /><br /><pre>" + reqStr + "</pre>" + "</body></html>";
+	responseStr += "<!DOCTYPE HTML><html><head><title>ConcileTime</title></head><body><h1>ConcileTime</h1><h2>WARNING!</h2><br />";
+	responseStr += "Tenant callback endpoint only allows PUT and DELETE methods to facilitate subscribe/unsubscribe.<br />";
+	responseStr += "</body></html>";
 	res.status(200).send(responseStr);
 });
 
